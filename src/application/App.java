1f43cc79614e1faaa17a8d4fb5bb991772ad1a3b
André Igor <andreigormenezes@gmail.com>
2024-08-13 12:06:57 -0300

    Position

---
+++ src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+import board.Position;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Position position = new Position(4, 6);
+		System.out.println(position);
+
+	}
+
+}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -70,8 +70,8 @@ public class ChessMatch {
 		this.promoted = promoted;
 	}
 	
-	public ChessPiece[][] getPieces() {
-		
-	}
+//	public ChessPiece[][] getPieces() {
+//		
+//	}
 
 }
