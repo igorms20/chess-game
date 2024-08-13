5896de3d2512fd1a45695f0c02d2872a69cd58ff
André Igor <andreigormenezes@gmail.com>
2024-08-13 11:57:33 -0300

    First commit

---
+++ src/chess/ChessMatch.java
@@ -0,0 +1,77 @@
+package chess;
+
+public class ChessMatch {
+
+	private int turn;
+	private Color currentPlayer;
+	private boolean check;
+	private boolean checkMate;
+	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
+
+	public ChessMatch() {
+	}
+
+	public ChessMatch(int turn, Color currentPlayer, boolean check, boolean checkMate, ChessPiece enPassantVulnerable,
+			ChessPiece promoted) {
+		this.turn = turn;
+		this.currentPlayer = currentPlayer;
+		this.check = check;
+		this.checkMate = checkMate;
+		this.enPassantVulnerable = enPassantVulnerable;
+		this.promoted = promoted;
+	}
+
+	public int getTurn() {
+		return turn;
+	}
+
+	public void setTurn(int turn) {
+		this.turn = turn;
+	}
+
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+	public void setCurrentPlayer(Color currentPlayer) {
+		this.currentPlayer = currentPlayer;
+	}
+
+	public boolean isCheck() {
+		return check;
+	}
+
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+
+	public boolean isCheckMate() {
+		return checkMate;
+	}
+
+	public void setCheckMate(boolean checkMate) {
+		this.checkMate = checkMate;
+	}
+
+	public ChessPiece getEnPassantVulnerable() {
+		return enPassantVulnerable;
+	}
+
+	public void setEnPassantVulnerable(ChessPiece enPassantVulnerable) {
+		this.enPassantVulnerable = enPassantVulnerable;
+	}
+
+	public ChessPiece getPromoted() {
+		return promoted;
+	}
+
+	public void setPromoted(ChessPiece promoted) {
+		this.promoted = promoted;
+	}
+	
+	public ChessPiece[][] getPieces() {
+		
+	}
+
+}
