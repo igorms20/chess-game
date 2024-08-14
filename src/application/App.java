bd909030ddec9da32c88e7f9e1dad3d7f1689171
André Igor <andreigormenezes@gmail.com>
2024-08-13 22:31:07 -0300

    Board created

--- src/application/App.java
+++ src/application/App.java
@@ -1,13 +1,15 @@
 package application;
 
+import board.Board;
 import board.Position;
 
 public class App {
 
 	public static void main(String[] args) {
 		
-		Position position = new Position(4, 6);
-		System.out.println(position);
+		Board board = new Board(8, 8);
+		
+		
 
 	}
 
