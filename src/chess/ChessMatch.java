010e47323e13f43193a9c4feee3a6b8873030c82
André Igor <andreigormenezes@gmail.com>
2024-09-02 12:20:30 -0300

    Printing possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -106,6 +106,13 @@ public class ChessMatch {
 
 	}
 
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+
+		return board.piece(position).possibleMoves();
+	}
+
 	private Piece makeMove(Position source, Position target) {
 		Piece piece = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
